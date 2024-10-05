ec9d3fabf473030caa311c2671c88f1e6deb68bc
shizuketsu <evdokimovnikita501@gmail.com>
2024-10-05 09:29:41 +0300

    thumbnail

--- src/commands/level.js
+++ src/commands/level.js
@@ -3,6 +3,22 @@ const { EmbedBuilder } = require('discord.js');
 const axios = require('axios');
 const qs = require('querystring');
 
+function setDifficult(diff, rate) {
+    if(diff === 1) diff = 'auto';
+    else if(diff === 2) diff = 'easy';
+    else if(diff === 3) diff = 'normal';
+    else if(diff === 4) diff = 'hard4';
+    else if(diff === 5) diff = 'hard5';
+    else if(diff === 6) diff = 'harder6';
+    else if(diff === 7) diff = 'harder7';
+    else if(diff === 8) diff = 'insane8';
+    else if(diff === 9) diff = 'insane9';
+    else if(diff === 10) return rate ? `https://gcs.icu/WTFIcons/difficulties/${rate}/demon-hard.png` : `https://gcs.icu/WTFIcons/difficulties/stars/demon-hard.png`;
+    else diff = 'na';
+    
+    return rate ? `https://panel.fhgdps.com/dist/img/gd/difficulties/${diff}-${rate}.png` : `https://panel.fhgdps.com/dist/img/gd/difficulties/${diff}.png`;
+}
+
 module.exports = {
     name: 'level',
     async execute(msg, args) {
@@ -10,9 +26,10 @@ module.exports = {
         let lvlData;
         let lvlLength;
         let likeEmoji;
+        let lvlDiff = 'https://panel.fhgdps.com/dist/img/gd/difficulties/na.png';
 
         try {
-            r = await axios.post(params.database + '/getGJLevels21.php?json', qs.stringify({
+            const r = await axios.post(params.database + '/getGJLevels21.php?json', qs.stringify({
                 'gameVersion': 22,
                 'binaryVersion': 41,
                 'gdw': 0,
@@ -71,10 +88,14 @@ module.exports = {
 
         likeEmoji = lvlData.likes > -1 ? '<:like:1291805792374362152>' : '<:dislike:1291805506880667688>';
 
+        if(lvlData.is_epic > 0) lvlDiff = setDifficult(lvlData.stars_got, 'epic');
+        else if(lvlData.is_featured > 0) lvlDiff = setDifficult(lvlData.stars_got, 'featured');
+
         const embed = new EmbedBuilder()
             .setColor('#92a2ff')
             .setTitle('Найден уровень')
-            .setDescription(`<:next:1291806046062514258> **${lvlData.name}** by __${lvlData.username}__\n<:info:1291805710673383424> ID: __${lvlData.id}__\n<:length:1291805762410254387> \`${lvlLength}\` <:downloads:1291805540355149935>\`${lvlData.downloads}\` ${likeEmoji} \`${Math.abs(lvlData.likes)}\``)
+            .setThumbnail(lvlDiff)
+            .setDescription(`<:play:1291806080900399188> **${lvlData.name}** by __${lvlData.username}__\n<:info:1291805710673383424> ID: \`${lvlData.id}\`\n<:length:1291805762410254387> \`${lvlLength}\` ${likeEmoji} \`${Math.abs(lvlData.likes)}\` <:downloads:1291805540355149935>\`${lvlData.downloads}\``)
             .setFooter({ text: 'Отправил ' + msg.author.username, iconURL: msg.author.displayAvatarURL() });
 
         await msg.channel.send({ embeds: [embed] });
